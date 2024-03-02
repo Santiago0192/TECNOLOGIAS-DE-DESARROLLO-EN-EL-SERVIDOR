4c9aee4e87f11621d1d73cadbb04e60fa059055d
Santiago-gluthe <112193745+Santiago-gluthe@users.noreply.github.com>
2024-03-02 15:49:09 -0600

    Tarea 1 (Merge attempt)
    Cree mal los branches entonces tuve que hacer el merge manual, se solucionara en futuros merge

---
+++ Tareas/tarea1/index.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+require('dotenv').config();
+const routes = require('./src/routes');
+
+const app = express();
+
+app.use(routes);
+
+let port=process.env.PORT || 3000;
+
+app.listen(port, () => {
+    if(process.env.NODE_ENV  === 'dev') {
+        console.log('App is running in port '+ port);
+    }
+    else {
+        console.log('App is running');
+    }
+});
