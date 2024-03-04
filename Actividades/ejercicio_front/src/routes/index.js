4e65601de8ddb6877252fa0792488a2b4409116a
Santiago0192 <94256359+Santiago0192@users.noreply.github.com>
2024-03-04 10:51:19 -0600

    04_03_2024

--- Actividades/ejercicio_front/src/routes/index.js
+++ Actividades/ejercicio_front/src/routes/index.js
@@ -2,8 +2,31 @@ const router = require('express').Router();
 const path = require('path')
 
 router.get('',(req,res) => {
-    const url = path.join(__dirname,'..','views','index.html');
-    res.sendFile(url);
+    //const url = path.join(__dirname,'..','views','index.html');
+    //res.sendFile(url);
+    res.render('home', {
+        title: 'APP NAME'
+    });
 });
+
+router.get('/about',(req, res) => {
+    res.render('about');
+})
+
+// Traer JSON usanbdo axios y si no recive userId envien un error, sino trae el usuario consultado
+router.get('/tareas',(req, res) => {
+    const userId = req.query.user;
+    if (userId) {
+        const url = `https://jsonplaceholder.typicode.com/todos?userId=${userId}`;
+        axios.get(url).then(response => {
+            res.render('todos',{
+                userId: userId,
+                todos: response.data
+            });
+        }).catch();
+    } else {
+        res.render('todos');
+    }
+})
 
 module.exports = router;
